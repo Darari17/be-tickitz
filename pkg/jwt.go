42711513a7341bf4103ecd9365e0a5b354bfd5d6
farid <faridramadhan59@gmail.com>
2025-09-15 08:01:09 +0700

    feat: add pkg

---
+++ pkg/jwt.go
@@ -0,0 +1,59 @@
+package pkg
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserID uuid.UUID `json:"id"`
+	Role   string    `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func NewJWTClaims(userId uuid.UUID, role string) *Claims {
+	return &Claims{
+		UserID: userId,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 30)),
+			Issuer:    os.Getenv("JWT_ISSUER"),
+		},
+	}
+}
+
+func (c *Claims) GenToken() (string, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return "", errors.New("no secret found")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+	return token.SignedString([]byte(jwtSecret))
+}
+
+func (c *Claims) VerifyToken(token string) error {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return errors.New("no secret found")
+	}
+	parsedToken, err := jwt.ParseWithClaims(token, c, func(t *jwt.Token) (any, error) { return []byte(jwtSecret), nil })
+	if err != nil {
+		return err
+	}
+	if !parsedToken.Valid {
+		return jwt.ErrTokenExpired
+	}
+	iss, err := parsedToken.Claims.GetIssuer()
+	if err != nil {
+		return err
+	}
+	if iss != os.Getenv("JWT_ISSUER") {
+		return jwt.ErrTokenInvalidIssuer
+	}
+
+	return nil
+}
